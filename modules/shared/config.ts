de55005e07ec3f0e70ecd6e14ac3b4fdfbc2a02e
Hizkia Felix Winata <felix@hizkifw.me>
2021-02-20 03:10:16 +0800

    Search API

---
+++ modules/shared/config.ts
@@ -0,0 +1,4 @@
+export const ES_INDEX = "youtube-archive";
+export const ES_BACKEND_URL = process.env.ES_BACKEND_URL;
+export const ES_BASIC_USERNAME = process.env.ES_BASIC_USERNAME;
+export const ES_BASIC_PASSWORD = process.env.ES_BASIC_PASSWORD;
---
+++ pages/api/search.ts
@@ -0,0 +1,61 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {
+  ES_BACKEND_URL,
+  ES_BASIC_PASSWORD,
+  ES_BASIC_USERNAME,
+  ES_INDEX,
+} from "../../modules/shared/config";
+import axios from "axios";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const q = req.query.q || "";
+
+  const searchRes = await axios.request({
+    method: "get",
+    baseURL: ES_BACKEND_URL,
+    url: "/" + ES_INDEX + "/_search",
+    auth: {
+      username: ES_BASIC_USERNAME,
+      password: ES_BASIC_PASSWORD,
+    },
+    data: !q
+      ? undefined
+      : {
+          query: {
+            bool: {
+              should: [
+                {
+                  match: {
+                    title: {
+                      query: q,
+                      operator: "OR",
+                      fuzziness: "AUTO",
+                    },
+                  },
+                },
+                {
+                  match: {
+                    description: {
+                      query: q,
+                      operator: "OR",
+                      fuzziness: "AUTO",
+                    },
+                  },
+                },
+                {
+                  match: {
+                    channel_name: {
+                      query: q,
+                      operator: "OR",
+                      fuzziness: "AUTO",
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+  });
+
+  res.json(searchRes.data);
+};
