1be7da3a1f7b3e8205c731f03ac8a756016fa3ff
Hizkia Felix Winata <felix@hizkifw.me>
2021-02-20 16:01:44 +0800

    Add nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,17 @@
+import NProgress from "nprogress";
+import Router from "next/router";
 import SearchProvider from "../modules/shared/hooks/search/SearchProvider";
 import "../styles/tailwind.css";
+import "nprogress/nprogress.css";
+
+NProgress.configure({
+  showSpinner: false,
+  trickle: true,
+});
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
