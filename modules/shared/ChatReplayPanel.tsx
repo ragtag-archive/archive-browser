7f8d358154ee4a2fdc89ae7874b3c77e43e227e3
Hizkia Felix Winata <felix@hizkifw.me>
2021-02-21 20:48:10 +0800

    Auto load chat, add chat toggle

--- modules/shared/ChatReplayPanel.tsx
+++ modules/shared/ChatReplayPanel.tsx
@@ -11,6 +11,7 @@ export type ChatReplayPanelProps = {
 const ChatReplayPanel = (props: ChatReplayPanelProps) => {
   const [replayData, setReplayData] = React.useState<ChatMessage[]>(null);
   const [downloadProgress, setDownloadProgress] = React.useState(-1);
+  const [isChatVisible, setIsChatVisible] = React.useState(true);
   const refChatScrollDiv = React.useRef<HTMLDivElement>(null);
 
   const downloadChatData = async () => {
@@ -22,6 +23,13 @@ const ChatReplayPanel = (props: ChatReplayPanelProps) => {
     });
     setReplayData(data.data);
   };
+
+  /**
+   * Automatically download chat replay
+   */
+  React.useEffect(() => {
+    downloadChatData();
+  }, []);
 
   React.useEffect(() => {
     if (refChatScrollDiv.current)
@@ -45,7 +53,7 @@ const ChatReplayPanel = (props: ChatReplayPanelProps) => {
             <p>Click to Enable</p>
           </>
         ) : downloadProgress === 0 ? (
-          <p>Initializing...</p>
+          <p>Loading chat replay...</p>
         ) : (
           <p>Loaded {(downloadProgress / 1024 / 1024).toFixed(2)}MB</p>
         )}
@@ -53,14 +61,30 @@ const ChatReplayPanel = (props: ChatReplayPanelProps) => {
     );
 
   return (
-    <div
-      className="border border-gray-800 rounded h-96 overflow-y-scroll resize-y"
-      ref={refChatScrollDiv}
-    >
-      <ChatReplay
-        currentTimeSeconds={props.currentTimeSeconds}
-        replayData={replayData}
-      />
+    <div>
+      <div
+        className={
+          "border border-gray-800 rounded overflow-y-scroll resize-y transition-all duration-200 " +
+          (isChatVisible ? "h-96" : "h-8")
+        }
+        ref={refChatScrollDiv}
+      >
+        {isChatVisible ? (
+          <ChatReplay
+            currentTimeSeconds={props.currentTimeSeconds}
+            replayData={replayData}
+          />
+        ) : (
+          <div>Chat disabled</div>
+        )}
+      </div>
+      <button
+        type="button"
+        onClick={() => setIsChatVisible((now) => !now)}
+        className="w-full text-center bg-gray-900 hover:bg-gray-800 focus:ring focus:outline-none rounded transition duration-200"
+      >
+        Toggle chat
+      </button>
     </div>
   );
 };
