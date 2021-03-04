e47ec16037aec214e833e9c338f837376e253757
Hizkia Felix Winata <felix@hizkifw.me>
2021-03-04 23:32:19 +0800

    fix(Amplitude): remove console.log

--- modules/shared/libs/amplitude/AmplitudeProvider.tsx
+++ modules/shared/libs/amplitude/AmplitudeProvider.tsx
@@ -5,16 +5,11 @@ const AmplitudeProvider = (props: any) => {
   const logEvent = (eventName: string, eventProperties?: any) => {
     if (typeof window !== "undefined") {
       try {
-        console.log("Initializing amplitude");
         const amplitude = require("amplitude-js");
         amplitude.init(process.env.NEXT_PUBLIC_AMPLITUDE_KEY, undefined, {});
         amplitude.setVersionName(process.env.NEXT_PUBLIC_APP_VERSION);
         amplitude.logEvent(eventName, eventProperties);
-        console.log("Done");
-      } catch (ex) {
-        console.log("Error");
-        console.log(ex);
-      }
+      } catch (ex) {}
     }
   };
 
