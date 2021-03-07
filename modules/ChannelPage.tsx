751a3a3900d484af4a700fb4ca1769974a72ffde
Hizkia Felix Winata <felix@hizkifw.me>
2021-03-07 18:07:32 +0800

    Add profile image and youtube button on channel page

--- modules/ChannelPage.tsx
+++ modules/ChannelPage.tsx
@@ -4,6 +4,7 @@ import { ElasticSearchResult, VideoMetadata } from "./shared/database";
 import PageBase from "./shared/PageBase";
 import PaginatedResults from "./shared/PaginatedResults";
 import { DRIVE_BASE_URL } from "./shared/config";
+import { IconYouTube } from "./shared/icons";
 
 export type ChannelPageProps = {
   channelId: string;
@@ -18,6 +19,7 @@ export type ChannelPageProps = {
 const ChannelPage = (props: ChannelPageProps) => {
   const { channelId, channelName, ...rest } = props;
   const description = "Browse archived videos from " + channelName;
+  const profileImage = DRIVE_BASE_URL + "/" + channelId + "/profile.jpg";
   return (
     <PageBase>
       <Head>
@@ -30,10 +32,7 @@ const ChannelPage = (props: ChannelPageProps) => {
           property="og:url"
           content={"https://archive.ragtag.moe/channel/" + channelId}
         />
-        <meta
-          property="og:image"
-          content={DRIVE_BASE_URL + "/" + channelId + "/profile.jpg"}
-        />
+        <meta property="og:image" content={profileImage} />
         <meta property="twitter:card" content="summary" />
         <meta property="twitter:title" content={channelName} />
         <meta property="twitter:description" content={description} />
@@ -41,15 +40,34 @@ const ChannelPage = (props: ChannelPageProps) => {
           property="twitter:url"
           content={"https://archive.ragtag.moe/channel/" + channelId}
         />
-        <meta
-          property="twitter:image"
-          content={DRIVE_BASE_URL + "/" + channelId + "/profile.jpg"}
-        />
+        <meta property="twitter:image" content={profileImage} />
       </Head>
       <div>
         <div className="px-4 my-16">
+          <img
+            src={profileImage}
+            className="w-32 h-32 mx-auto mb-4 rounded-full"
+          />
           <p className="text-lg text-center">All videos from</p>
           <h1 className="text-3xl text-center">{channelName}</h1>
+          <div className="flex mt-2 justify-center">
+            <a
+              href={"https://youtube.com/channel/" + channelId}
+              target="_blank"
+              rel="noreferrer noopener"
+              className="
+                bg-gray-800
+                hover:bg-gray-700
+                focus:bg-gray-900 focus:outline-none
+                px-4 py-2 md:mr-2 mb-2 md:mb-0 rounded
+                transition duration-200
+                flex flex-row items-center
+              "
+            >
+              <IconYouTube className="w-4 h-4 mr-3" />
+              Watch on YouTube
+            </a>
+          </div>
         </div>
         <PaginatedResults {...rest} />
       </div>
