f5cb27d56e42068531ec5822d3a96f3de455cf55
Hizkia Felix Winata <felix@hizkifw.me>
2021-03-20 20:27:01 +0800

    feat(SeekBar): show timestamp on hover

--- modules/shared/VideoPlayer/SeekBar.tsx
+++ modules/shared/VideoPlayer/SeekBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { formatSeconds } from "../format";
 
 export type SeekBarProps = {
   value: number;
@@ -10,6 +11,7 @@ const SeekBar = (props: SeekBarProps) => {
   const { value, max, onChange } = props;
   const [isScrubbing, setIsScrubbing] = React.useState(false);
   const [hoverPercentX, setHoverPercentX] = React.useState(0);
+  const [isTimePopupVisible, setIsTimePopupVisible] = React.useState(false);
 
   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     const rect = e.currentTarget.getBoundingClientRect();
@@ -19,27 +21,40 @@ const SeekBar = (props: SeekBarProps) => {
   };
 
   return (
-    <div
-      className="w-full h-4 relative group cursor-pointer"
-      onMouseDown={() => {
-        setIsScrubbing(true);
-        onChange(hoverPercentX * max);
-      }}
-      onMouseUp={() => setIsScrubbing(false)}
-      onMouseMove={handleMouseMove}
-      aria-label="Seekbar"
-      aria-role="slider"
-      aria-valuenow={value}
-    >
-      <div className="absolute bottom-1 bg-white opacity-50 h-1 group-hover:h-2 w-full transition-all duration-200" />
+    <>
       <div
-        className="absolute bottom-1 bg-blue-500 h-1 group-hover:h-2"
-        style={{
-          transition: "height .2s",
-          width: (100 * value) / max + "%",
+        className="w-full h-4 relative group cursor-pointer"
+        onMouseDown={() => {
+          setIsScrubbing(true);
+          onChange(hoverPercentX * max);
         }}
-      />
-    </div>
+        onMouseUp={() => setIsScrubbing(false)}
+        onMouseMove={handleMouseMove}
+        onMouseOver={() => setIsTimePopupVisible(true)}
+        onMouseOut={() => setIsTimePopupVisible(false)}
+        aria-label="Seekbar"
+        aria-role="slider"
+        aria-valuenow={value}
+      >
+        <div className="absolute bottom-1 bg-white opacity-50 h-1 group-hover:h-2 w-full transition-all duration-200" />
+        <div
+          className="absolute bottom-1 bg-blue-500 h-1 group-hover:h-2"
+          style={{
+            transition: "height .2s",
+            width: (100 * value) / max + "%",
+          }}
+        />
+        <div
+          className="absolute -top-10 bg-black bg-opacity-50 px-2 py-1 rounded"
+          style={{
+            opacity: isTimePopupVisible ? 1 : 0,
+            left: hoverPercentX * 100 + "%",
+          }}
+        >
+          {formatSeconds(hoverPercentX * max)}
+        </div>
+      </div>
+    </>
   );
 };
 
